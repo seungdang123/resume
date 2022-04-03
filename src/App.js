9ccccf3ccffd77e71639e67eb3e7fb63a65466f7
seungdang123 <seungdang123@gmail.com>
2022-04-04 01:23:19 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import Paper from "./components/Paper";
+import styled from "styled-components";
+
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Paper>
+        <h1>Test</h1>
+      </Paper>
+    </Container>
   );
 }
 
---
+++ src/components/Paper.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+const Paper = styled.div`
+  height: 95vh;
+  width: 80vw;
+  border: 1px solid black;
+  background-color: whitesmoke;
+`;
+
+export default Paper;
